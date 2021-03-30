7a735e25a350f93d3d30f1d33a074dd058e45ad2
Athul Sanjose <59477486+UltimateRoman@users.noreply.github.com>
2021-03-30 12:04:49 +0530

    Initial sketches

---
+++ wifiscan_oled/wifiscan_oled.ino
@@ -0,0 +1,25 @@
+#include<ESP8266WiFi.h>
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  int n = WiFi.scanNetworks();
+  Serial.println("Available networks: ");
+  for(int i=0; i<n; ++i) {
+    Serial.print("\nSSID: ");
+    Serial.println(WiFi.SSID(i));
+
+    Serial.print("Strength: ");
+    Serial.println(WiFi.RSSI(i));
+
+    Serial.print("Channel: ");
+    Serial.println(WiFi.channel(i));
+
+    Serial.print("Encryption: ");
+    Serial.println(WiFi.encryptionType(i));
+    delay(100);
+  }
+  delay(10000);
+}
