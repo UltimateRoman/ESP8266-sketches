9840470acda857f6b65597eba1030096a809422b
Athul Sanjose <59477486+UltimateRoman@users.noreply.github.com>
2021-04-09 11:00:51 +0530

    Modified dht-webserver

--- Webserver_DHT/Webserver_DHT.ino
+++ Webserver_DHT/Webserver_DHT.ino
@@ -2,11 +2,14 @@
 #include<ESP8266WebServer.h>
 #include "DHT.h"
 
-#define WiFi_SSID "ssid"
-#define WiFi_PWD "password"
+#define WiFi_SSID "SSID"
+#define WiFi_PWD "Password"
 
 ESP8266WebServer server(80);
 DHT dht(D1, DHT11);
+
+float temp=0, hum=0;
+unsigned long t1=0;
 
 void setup() {
   Serial.begin(9600);
@@ -19,6 +22,8 @@ void setup() {
   server.on("/", show_temp);
   server.onNotFound(handle_notFound);
   server.begin();
+  dht.begin();
+  get_val();
 }
 
 void loop() {
@@ -26,12 +31,24 @@ void loop() {
 }
 
 void show_temp() {
-  float temp = dht.readTemperature();
-  float hum = dht.readHumidity();
+  if(millis()-t1>2000) {
+    get_val();
+  }
   String html = "<h1>Temperature: "+String(temp)+" C </h1> <br><br> <h1>Humditity: "+String(hum)+" %</h1>";
   server.send(200, "text/html", html);
 }
 
 void handle_notFound() {
   server.send(404, "text/plain", "Not found");
+}
+
+void get_val() {
+  t1 = millis();
+  float t, h;
+  t = dht.readTemperature();
+  h = dht.readHumidity();
+  if((!isnan(t))&&(!isnan(h))) {
+    temp=t;
+    hum=h;
+  }
 }
